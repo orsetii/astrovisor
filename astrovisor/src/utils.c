47d110b4b16a7f81942c408973f1663836d1b1a8
orseti <hello@orseti.com>
2021-05-19 03:44:36 +0100

    Small Fixes

--- astrovisor/src/utils.c
+++ astrovisor/src/utils.c
@@ -1,27 +1,33 @@
 #include "utils.h"
+#include <stdarg.h>
 
 void dbgprint (_In_ PCCH Format, ...)
 {
-    va_list arglist;
+    va_list arglist = { 0 };
 
     va_start(arglist, Format);
-    vDbgPrintEx(DPFLTR_IHVDRIVER_ID, DPFLTR_ERROR_LEVEL, Format, arglist);
+    vDbgPrintExWithPrefix("[*]", DPFLTR_IHVDRIVER_ID, DPFLTR_ERROR_LEVEL, Format, arglist);
     va_end(arglist);
 }
 
-void get_physical_address(_In_ PVOID BaseAddress)
+ULONGLONG get_physical_address(_In_ PVOID BaseAddress)
 {
     return MmGetPhysicalAddress(BaseAddress).QuadPart;
 }
 
-int get_proc_count()
+int get_proc_count(void)
 {
     return KeQueryActiveProcessorCountEx(ALL_PROCESSOR_GROUPS);
 }
 
-int get_proc_number() 
+int get_proc_number(void) 
 {
     return KeGetCurrentProcessorNumberEx(NULL);
+}
+
+void AstroUnload(void)
+{
+    // TODO!
 }
 
 // TODO look into adding 'CONTEXT' functions to restore state, its a windows structure.
