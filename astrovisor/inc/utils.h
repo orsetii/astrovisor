6e8e93d3c5453f76a0870003bb0b126188307520
orseti <hello@orseti.com>
2021-05-20 00:13:27 +0100

    Added `TAG` for when allocating tagged memory, and created two functions to alloc and free such tagged memory

--- astrovisor/inc/utils.h
+++ astrovisor/inc/utils.h
@@ -3,7 +3,22 @@
 #include <ntddk.h>
 #include <wdm.h>
 
-ULONGLONG get_physical_address(_In_ PVOID BaseAddress);
+#define TAG 'ASTR'
+
+PHYSICAL_ADDRESS get_physical_address(_In_ PVOID BaseAddress);
+
+__drv_allocatesMem(Mem)
+_Post_writable_byte_size_(NumberOfBytes)
+_Post_maybenull_
+_IRQL_requires_max_(DISPATCH_LEVEL)
+_IRQL_requires_same_
+_Must_inspect_result_
+void* alloc_pagealigned_physical_memory_with_tag(_In_ SIZE_T NumberOfBytes);
+
+_IRQL_requires_max_(DISPATCH_LEVEL)
+_IRQL_requires_same_
+void free_pagealigned_physical_memory_with_tag(_Pre_notnull_ __drv_freesMem(Mem) PVOID BaseAddress);
+
 int get_proc_number(void);
 int get_proc_count(void);
 void dbgprint(_In_ PCCH Format, ...);
--- astrovisor/src/utils.c
+++ astrovisor/src/utils.c
@@ -10,9 +10,9 @@ void dbgprint (_In_ PCCH Format, ...)
     va_end(arglist);
 }
 
-ULONGLONG get_physical_address(_In_ PVOID BaseAddress)
+PHYSICAL_ADDRESS get_physical_address(_In_ PVOID BaseAddress)
 {
-    return MmGetPhysicalAddress(BaseAddress).QuadPart;
+    return MmGetPhysicalAddress(BaseAddress);
 }
 
 int get_proc_count(void)
@@ -30,4 +30,36 @@ void AstroUnload(void)
     // TODO!
 }
 
+
 // TODO look into adding 'CONTEXT' functions to restore state, its a windows structure.
+
+// A copied function, but seems well optimized.
+__drv_allocatesMem(Mem)
+_Post_writable_byte_size_(NumberOfBytes)
+_Post_maybenull_
+_IRQL_requires_max_(DISPATCH_LEVEL)
+_IRQL_requires_same_
+_Must_inspect_result_
+void* alloc_pagealigned_physical_memory_with_tag(_In_ SIZE_T NumberOfBytes)
+{
+     void* memory;
+
+    // The size must be equal or greater than PAGE_SIZE in order to allocate
+    // page aligned memory.
+    NT_ASSERT(NumberOfBytes >= PAGE_SIZE);
+
+    memory = ExAllocatePoolWithTag(NonPagedPool, NumberOfBytes, TAG);
+    if (memory != NULL)
+    {
+        NT_ASSERT(PAGE_ALIGN(memory) == memory);
+        RtlSecureZeroMemory(memory, NumberOfBytes);
+    }
+    return memory;
+}
+
+_IRQL_requires_max_(DISPATCH_LEVEL)
+_IRQL_requires_same_
+void free_pagealigned_physical_memory_with_tag(_Pre_notnull_ __drv_freesMem(Mem) PVOID BaseAddress)
+{
+    ExFreePoolWithTag(BaseAddress, TAG);
+}
